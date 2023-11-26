397a7be3781ecc872f413379fc3c05b380f3bc92
Rospaccio <alberto.mercati@gmail.com>
2023-11-26 11:45:46 +0100

    Add web pages for administration (WIP)

--- combo-web/src/main/java/xyz/codevomit/combo/web/ComboWebConfiguration.java
+++ combo-web/src/main/java/xyz/codevomit/combo/web/ComboWebConfiguration.java
@@ -2,14 +2,17 @@ package xyz.codevomit.combo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import xyz.codevomit.combo.bot.Combo;
 import xyz.codevomit.combo.data.DataService;
+import xyz.codevomit.combo.web.console.HelloController;
 
 @Configuration
+@ComponentScan(basePackageClasses = HelloController.class)
 public class ComboWebConfiguration {
 
     @Bean
